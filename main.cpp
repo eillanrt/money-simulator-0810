277d98a1aba373b3851952fa9891d4ae65c3b14d
Allan Robert Tan <argt@duck.com>
2023-12-08 09:16:37 +0000

    create gamble function

--- main.cpp
+++ main.cpp
@@ -1,68 +1,168 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-class Item {
-    private: 
-      double price;
-
-};
-
 class MoneySimulator {
-    protected:
-      double moneyAmount;
+  protected: double moneyAmount;
 
-   public:
-     MoneySimulator(double moneyAmount) {
-       this->moneyAmount = moneyAmount;
+  private:
+    bool stringExistsInArray(const std::string & str,
+      const std::string arr[], size_t size) {
+      return std::find(arr, arr + size, str) != arr + size;
     }
 
-    void homeScreen() {
-       string activities[] = {"Gamble", "Go to bank"};
-       int numberOfActivities = sizeof(activities)/sizeof(activities[0]);
-       int activityChoice;
+  int random(int min, int max) { //range : [min, max]
 
-       bool proceedToActivity = true;
+    static bool first = true;
 
-       while (proceedToActivity) {
-          cout << "What would you like to do? \n PICK A NUMBER\n\n";
-       
-       for (int i = 0; i < numberOfActivities; i++) {
-         cout << i << " -> " << activities[i] << endl;
-       }
+    if (first) {
+      srand(time(NULL)); //seeding for the first time only!
+      first = false;
+    }
+    return min + rand() % ((max + 1) - min);
+  }
 
-         cin >> activityChoice;
-         proceedToActivity = activityChoice > numberOfActivities || activityChoice < 0;
-       }
+  public: MoneySimulator(double moneyAmount) {
+    this -> moneyAmount = moneyAmount;
+  }
 
-       switch (activityChoice) {
-        case 0:
-          gamble();
-       }
+  void homeScreen() {
+   clearTerminal();
+   cout << "----------------------------- HOME ------------------------------------\n";
+    string activities[] = {
+      "Gamble",
+      "Go to bank"
+    };
+
+    int numberOfActivities = sizeof(activities) / sizeof(activities[0]);
+    int activityChoice;
+
+    bool proceedToActivity = true;
+
+    while (proceedToActivity) {
+
+      for (int i = 0; i < numberOfActivities; i++) {
+        cout << i << " -> " << activities[i] << endl;
+      }
+
+      cout << "What would you like to do?\n\nPICK A NUMBER\n\n=> ";
+
+      cin >> activityChoice;
+      proceedToActivity = activityChoice > numberOfActivities || activityChoice < 0;
     }
 
-     void gamble() {
-         // implement gamble
-     }
+    switch (activityChoice) {
+    case 0:
+      gamble();
+      break;
+    }
+  }
 
-     void start() {
-         cout << "Game is starting..." << endl;
+  void gamble() {
+   clearTerminal();
+      cout << "----------------------------- CASINO ------------------------------------\n";
+
+    double moneyToGamble;
+    cout << "You have ₱" << moneyAmount << "\n";
+    cout << "Money you want to bet: ";
+    cin >> moneyToGamble;
+
+    if (moneyToGamble > moneyAmount) {
+      cout << "\nYou don't have enough money to bet that much\n\nContinue? 1=yes 0=no" << endl;
+      bool conti;
+      cin >> conti;
+
+      if (!conti){
+         system("clear");
          homeScreen();
-     }
+      } else {
+         system("clear");
+         gamble();
+         return;
+      }
+    }
 
-     void addMoney(double amount){
-        moneyAmount += amount;
-     }
+    string colors[] = {
+      "blue",
+      "green",
+      "red",
+      "yellow"
+    };
 
-      void reduceMoney(double amount){
-        moneyAmount -= amount;
-     }
+    int howManyColors = sizeof(colors) / sizeof(colors[0]);
 
-     double getMoney() { return moneyAmount; }
+    bool colorChosenIsValid = true;
+    string chosenColor;
+
+    do {
+      if (!colorChosenIsValid) {
+         
+         cout << "INVALID COLOR PICK AGAIN!\n\n";
+      }
+
+      cout << "Choose a color: ";
+
+      for (const string color: colors) {
+        cout << color << " ";
+      }
+      cout << "\n";
+      cin >> chosenColor;
+      colorChosenIsValid = stringExistsInArray(chosenColor, colors, howManyColors);
+    } while (!colorChosenIsValid);
+
+    string randomColor = colors[random(0, 2)];
+
+    cout << "YOUR COLOR: " << chosenColor << "\nRANDOM COLOR: " << randomColor << "\n";
+
+    if (randomColor == chosenColor) {
+      moneyToGamble  *= 2;
+      cout << "\nCONGRATULATIONS YOUR MONEY JUST GOT DOUBLED!!\n";
+    } else {
+      moneyAmount -= moneyToGamble;
+     cout << "\nBETTER LUCK NEXT TIME!!\n\nYOU LOSE " << moneyToGamble << "\n";
+    }
+
+    cout << "You have now ₱" << moneyAmount << "\n\n";
+
+    bool goHome;
+    cout << "Go back to home screen? \n 1=yes 0=Gamble again\n=>";
+    cin >> goHome;
+
+    if (goHome) {
+      homeScreen();
+    } else {
+      gamble();
+    }
+  }
+
+  void start() {
+    cout << "Game is starting..." << endl;
+    homeScreen();
+  }
+
+  void addMoney(double amount) {
+    moneyAmount += amount;
+  }
+
+  void reduceMoney(double amount) {
+    moneyAmount -= amount;
+  }
+
+  double getMoney() {
+    return moneyAmount;
+  }
+   void clearTerminal() {
+      system("clear");
+    }
+
 };
 
 int main() {
-   MoneySimulator app(100);
-   app.start();
+  MoneySimulator app(100);
 
-    return 0;
+  app.clearTerminal();
+  app.start();
+
+  return 0;
 }
